3d578b1915a79f13231b28b9aca6a13ff23b713e
Nicco Kunzmann <niccokunzmann@rambler.ru>
2017-07-26 03:23:24 -0700

    change travis test

--- scripts/test_api.sh
+++ scripts/test_api.sh
@@ -3,11 +3,11 @@ set -e
 
 cd "`dirname \"$0\"`"
 
-go build github.com/schul-cloud/arix-search-adapter/search
-./search &
+go build github.com/schul-cloud/arix-search-adapter
+./arix-search-adapter &
 PID="$!"
 
 python -m schul_cloud_search_tests.search \
-        --url=http://localhost:8080/v1/search
+       --url=http://localhost:8080/v1/search
 
 kill "$PID"
